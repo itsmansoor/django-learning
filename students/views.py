d516d73041caaad44e8ba9503b98fc5e41f1c628
itsmansoor <mansoorkhaksar22@gmail.com>
2026-06-24 11:41:24 +0500

    Completed Django Phase 2: templates, navigation, CSS, and static files

--- students/views.py
+++ students/views.py
@@ -1,4 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello Django")
+    return render(request, "home.html",{
+         "name": "Muhammad Mansoor",
+        "course": "Django"
+    })
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
