732b0e141db49983117a8a5aba0c2ab12447a23e
itsmansoor <mansoorkhaksar22@gmail.com>
2026-06-25 11:56:43 +0500

    Completed Django Phase 3: models, migrations, ORM and CRUD operations

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import home, about, contact
+from .views import home, about, contact,students_list,create_student,single_student,filter_students,update_student,delete_student
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
+    path("students/", students_list, name="students"),
+    path("create/", create_student),
+    path("student/", single_student),
+    path("filter/", filter_students),
+    path("update/", update_student),
+    path("delete/", delete_student),
 ]
--- students/views.py
+++ students/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Student
 
 def home(request):
     return render(request, "home.html",{
@@ -11,3 +12,55 @@ def about(request):
 
 def contact(request):
     return render(request, "contact.html")
+
+
+
+def students_list(request):
+    students = Student.objects.all()
+
+    return render(request, "students.html", {
+        "students": students
+    })
+
+def create_student(request):
+
+    Student.objects.create(
+        name="Ali",
+        age=22,
+        email="ali@gmail.com"
+    )
+
+    return render(request, "success.html")
+
+def single_student(request):
+
+    student = Student.objects.get(id=1)
+
+    return render(request, "single_student.html", {
+        "student": student
+    })
+
+def filter_students(request):
+
+    students = Student.objects.filter(age=22)
+
+    return render(request, "students.html", {
+        "students": students
+    })
+
+def update_student(request):
+
+    student = Student.objects.get(id=1)
+
+    student.name = "Muhammad Mansoor"
+    student.save()
+
+    return render(request, "success.html")
+
+def delete_student(request):
+
+    student = Student.objects.get(id=1)
+
+    student.delete()
+
+    return render(request, "success.html")
